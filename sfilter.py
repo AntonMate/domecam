4270e4c9bf720c2c17e9f51c4afe11cc3c40add5
Anton m <115307728+AntonMate@users.noreply.github.com>
2023-01-30 17:25:27 +0300

    reload

--- sfilter.py
+++ sfilter.py
@@ -2,14 +2,14 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sfilter(D=None, D_pix=None, z=None):
-    spectral_filter = pd.read_csv("C:/Users/miron/Downloads/KC19_d16t4_Safonov.csv", sep = ';')
+def sfilter(D=None, Dpix=None, z=None):
+    spectral_filter = pd.read_csv("KC19_d16t4_Safonov.csv", sep = ';')
     spectral_filter['14.42.43'] = spectral_filter['14.42.43'] / 100
 
-    ccd_QE = pd.read_csv("C:/Users/miron/Downloads/ccd_prosilica2.crv", sep = ' ')
+    ccd_QE = pd.read_csv("ccd_prosilica2.crv", sep = ' ')
     ccd_QE['X'] = ccd_QE['X'] * 1000
 
-    star = pd.read_csv("C:/Users/miron/Downloads/a05.sp", sep = ' ')
+    star = pd.read_csv("a05.sp", sep = ' ')
     
     # на данном этапе мы обрезаем значения файлов так, чтобы они покрывались всеми тремя файлами
     tmp_min = np.max([ccd_QE['X'][0], star['X'][0], spectral_filter['WAVE_LENGTH'][0]])
@@ -27,7 +27,7 @@ def sfilter(D=None, D_pix=None, z=None):
     X3 = star['X'] * pow(10, -9) # [м]
     Y3 = star['Y'] 
 
-    coeff = 1
+    coeff = 100
     k = 1000*coeff
     max_lambda = tmp_max*coeff
     lambdas = np.linspace(0, max_lambda, k) * pow(10, -9) # [м]
@@ -43,9 +43,9 @@ def sfilter(D=None, D_pix=None, z=None):
     omega_lambdas_scale = 1 / (delta_lambdas) # максималльноешаг по частоте, [м^-1]
     res_fft = pow((np.imag(np.fft.fft(result/lambdas))), 2)
 
-    nx = 512 # размер окна, [n_pix]
+    nx = 2 * Dpix + 2
 
-    delta = D/D_pix # шаг субапертуры, период дискретизации (то, насколько одно значение отстает от следующего) [м]
+    delta = D/Dpix # шаг субапертуры, период дискретизации (то, насколько одно значение отстает от следующего) [м]
     
     f_scale = 1/(delta*nx) # шаг по частоте, [м^-1]
     xx, yy = np.meshgrid(np.linspace(-nx//2, nx//2-1, nx), np.linspace(-nx//2, nx//2-1, nx))
