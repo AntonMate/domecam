d66a18ae48247ad6c2da5ae0b97fba4119360605
Anton m <115307728+AntonMate@users.noreply.github.com>
2023-02-06 19:33:28 +0300

    reload

--- sfilter.py
+++ sfilter.py
@@ -1,53 +1,21 @@
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 
-def sfilter(D=None, Dpix=None, z=None):
-    spectral_filter = pd.read_csv("KC19_d16t4_Safonov.csv", sep = ';')
-    spectral_filter['14.42.43'] = spectral_filter['14.42.43'] / 100
-
-    ccd_QE = pd.read_csv("ccd_prosilica2.crv", sep = ' ')
-    ccd_QE['X'] = ccd_QE['X'] * 1000
-
-    star = pd.read_csv("a05.sp", sep = ' ')
-    
-    # на данном этапе мы обрезаем значения файлов так, чтобы они покрывались всеми тремя файлами
-    tmp_min = np.max([ccd_QE['X'][0], star['X'][0], spectral_filter['WAVE_LENGTH'][0]])
-    tmp_max = np.min([ccd_QE['X'].iat[-1], star['X'].iat[-1], spectral_filter['WAVE_LENGTH'].iat[-1]])
-
-    spectral_filter = spectral_filter[spectral_filter['WAVE_LENGTH'] < tmp_max]
-    X1 = spectral_filter['WAVE_LENGTH'] * pow(10, -9) # [м]
-    Y1 = spectral_filter['14.42.43']
-
-    ccd_QE = ccd_QE[ccd_QE['X'] > tmp_min]
-    X2 = ccd_QE['X'] * pow(10, -9) # [м]
-    Y2 = ccd_QE['Y']
-
-    star = star[star['X'] > tmp_min]
-    X3 = star['X'] * pow(10, -9) # [м]
-    Y3 = star['Y'] 
-
-    coeff = 100
+def sfilter(coeff, lambda_max=None, f_lambda=None, D=None, Dpix=None, z=None):
     k = 1000*coeff
-    max_lambda = tmp_max*coeff
-    lambdas = np.linspace(0, max_lambda, k) * pow(10, -9) # [м]
+    lambda_max_new = int(lambda_max*coeff)
+    lambdas = np.linspace(0, lambda_max_new, k) * pow(10, -9) # [м]
 
-    interpolate_df = np.interp(lambdas, X1, Y1)
-    interpolate_ccd = np.interp(lambdas, X2, Y2)
-    interpolate_a05 = np.interp(lambdas, X3, Y3)
-    result = interpolate_df * interpolate_ccd * interpolate_a05
-    result = result/np.sum(result)
+    tail = np.zeros((len(lambdas) - len(f_lambda)))
+    f_lambda_new = np.append(f_lambda, tail)
+    res_fft = pow((np.imag(np.fft.fft(f_lambda_new/lambdas))), 2)
 
-    # мнимая часть Фурье преобразования от функции спектрального отклика
-    delta_lambdas = (max_lambda / len(lambdas)) * pow(10, -9) # период дискредизации, [м]
-    omega_lambdas_scale = 1 / (delta_lambdas) # максималльноешаг по частоте, [м^-1]
-    res_fft = pow((np.imag(np.fft.fft(result/lambdas))), 2)
+    delta_lambdas = (lambda_max_new / len(lambdas)) * pow(10, -9) # период дискретизации, шаг по частоте [м]
+    omega_lambdas_scale = 1 / (delta_lambdas) # максимальное значение по частоте, [м^-1]
 
-    nx = 2 * Dpix + 1
-
+    nx = 2*Dpix+1 # размер окна, [n_pix]
     delta = D/Dpix # шаг субапертуры, период дискретизации (то, насколько одно значение отстает от следующего) [м]
-    
     f_scale = 1/(delta*nx) # шаг по частоте, [м^-1]
+
     xx, yy = np.meshgrid(np.linspace(-nx//2, nx//2-1, nx), np.linspace(-nx//2, nx//2-1, nx))
     xx_scale = f_scale * xx 
     yy_scale = f_scale * yy
@@ -58,4 +26,4 @@ def sfilter(D=None, Dpix=None, z=None):
     omega_new = np.interp(omega, np.linspace(0, omega_lambdas_scale, k), res_fft)
     omega_new = np.resize(omega_new, (nx, nx))
     
-    return omega_new   
+    return omega_new
