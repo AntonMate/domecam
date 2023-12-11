f3ae897c19c77fb922db07a67483c9b46fe262c7
Anton m <115307728+AntonMate@users.noreply.github.com>
2023-12-11 14:37:28 +0300

    Add files via upload

--- approx.py
+++ approx.py
@@ -6,6 +6,8 @@ import pandas as pd
 from scipy.ndimage import shift
 from scipy.optimize import curve_fit
 
+from skimage.filters import gaussian
+
 def circle(radius, size, circle_centre=(0, 0), origin="middle"):
     """
     Create a 2-D array: elements equal 1 within a circle and 0 outside.
@@ -93,9 +95,13 @@ def circle(radius, size, circle_centre=(0, 0), origin="middle"):
     # (5) Return:
     return C
 
-def processApprox(cc=None, gammas=None, lambda_=None, D=None, latency=None, sec_per_frame=None, cjk=None, initial_params=None, all_Vx=None, all_Vy=None, all_Cn2_bounds=None, conjugated_distance=None, num_of_layers=None, heights_of_layers=None, dome_index=None):
+def processApprox(cc=None, gammas=None, lambda_=None, D=None, latency=None, sec_per_frame=None, cjk=None, initial_params=None, all_Vx=None, all_Vy=None, all_Cn2_bounds=None, conjugated_distance=None, num_of_layers=None, heights_of_layers=None, dome_index=None, use_gradient=False, do_fitting=True, dome_only=None, use_windvar=None):
     print(' - initial guess for the parameters:')
-    df_ip = pd.DataFrame(initial_params, columns = ['Vx, m/s','Vy, m/s','Cn2', 'z, m']) 
+    if use_windvar:
+        df_ip = pd.DataFrame(initial_params, columns = ['Vx, m/s','Vy, m/s','Cn2', 'z, m', 'var, m/s']) 
+    else:
+        df_ip = pd.DataFrame(initial_params, columns = ['Vx, m/s','Vy, m/s','Cn2', 'z, m']) 
+        
     df_ip = df_ip.sort_values(by=['z, m'])
     df_ip = df_ip.reset_index()
     df_ip['Cn2'] = df_ip['Cn2']*1e-13
@@ -105,11 +111,15 @@ def processApprox(cc=None, gammas=None, lambda_=None, D=None, latency=None, sec_
     df_ip = df_ip.round({'z, m': 2})
     df_ip.drop(columns=['index'], inplace=True)
     print(df_ip.to_string(index=False))
-    
-    t = latency * sec_per_frame
-    delta = D/(cc.shape[0]//2)
-    t_delta = t/delta
-    
+  
+    def fitconvert(fit1d,shape):
+        n_elem=shape[1]*shape[2]
+        fit = np.ndarray(shape)
+        for idx in range(len(latency)):
+            tmp = fit1d[idx*n_elem:(idx+1)*n_elem]
+            fit[idx] = tmp.reshape((shape[1],shape[2]))
+        return fit
+
     def find_nearest(array, value):
         # array = np.asarray(array)
         idx = (np.abs(array - value)).argmin()
@@ -124,69 +134,173 @@ def processApprox(cc=None, gammas=None, lambda_=None, D=None, latency=None, sec_
             if array[idx] < value:
                 return idx+1, idx
 
-    def gamma_se(X, Y, Vx, Vy, Cn2, z): 
-        Cn2=Cn2*1e-13
+    def gamma_se1(X, Y, t_delta, Vx, Vy, Cn2, z): 
+#        Cn2=Cn2*1e-13 # БС: это не обязательно делать, см. комментарий в конце этой функции
         z=z*1000
-        
+
         uv, lv = find_nearest(heights_of_layers, z)
         res = gammas[lv] + (z - heights_of_layers[lv])*((gammas[uv] - gammas[lv])/(heights_of_layers[uv] - heights_of_layers[lv]))
-        
-#         Lx = Vx*t
-#         Ly = Vy*t
-#         Xpix = Lx/delta
-#         Ypix = Ly/delta
         
         Xpix = Vx*t_delta
         Ypix = Vy*t_delta
         res = shift(res, (-Ypix, Xpix), order=1)  
+# БС: здесь был баг - не было умножения на Cn2, из-за чего Cn2 вообще не подбиралось
+        return res*Cn2 
+
+    def gamma_se2(X, Y, t_delta, Vx, Vy, Cn2, z, Vsigma): 
+        z=z*1000
+
+        uv, lv = find_nearest(heights_of_layers, z)
+        res = gammas[lv] + (z - heights_of_layers[lv])*((gammas[uv] - gammas[lv])/(heights_of_layers[uv] - heights_of_layers[lv]))
+        
+        Xpix = Vx*t_delta
+        Ypix = Vy*t_delta
+        res = shift(res, (-Ypix, Xpix), order=1)  
+        res = res * Cn2
+        res = gaussian(res, sigma=Vsigma*t_delta)
         return res
 
-    def one_speckle_fit(initial_params=None, data=None, lambda_=None, all_Vx=None, all_Vy=None, all_Cn2_bounds=None, conjugated_distance=None, dome_index=None): 
-        def _g(M, *args): 
-            x, y = M
-            arr = np.zeros(x.shape, dtype=np.float32)
-            for i in range(len(args)//4):
-                arr += gamma_se(x, y, *args[i*4:i*4+4]).ravel()
-            return arr
+    def one_speckle_fit(initial_params=None, data=None, latency=None, lambda_=None, all_Vx=None, all_Vy=None, all_Cn2_bounds=None, conjugated_distance=None, dome_index=None, use_gradient=None, do_fitting=None, dome_only=None, use_windvar=None): 
+        class _g:
+
+            def __init__(self):
+                pass
+
+            def fitfun(self, M, *args):
+                xi, yi = M
+
+                n_elem = int(xi.shape[0]/len(latency))
+                array_size = int(np.sqrt(n_elem))
+                x = xi[:n_elem]
+                y = yi[:n_elem]
+
+# БС: Собираем модельную кросс-корреляцию для разных задержек
+                total_cc = np.zeros(xi.shape)
+                for idx in range(len(latency)):
+                    delta = D/(array_size//2)
+                    t_delta = latency[idx] * sec_per_frame/delta
+
+                    if self.use_gradient: 
+                        # БС: Модификация функции _g, моделирующая градиенты между турбулентными слоями
+                        # путем кусочной линейной интерполяции 
+                        arr = np.zeros(x.shape, dtype=np.float32)
+                        Nsublayer = 15
+                        arr += gamma_se1(x, y, t_delta, args[0], args[1], args[2], args[3]).ravel() # БС: между первым и вторым слоем градиент не делаем
+                        for i in range(1,len(args)//4-1):
+                            Vx_range  = np.linspace(args[i*4],  args[i*4+4],Nsublayer)
+                            Vy_range  = np.linspace(args[i*4+1],args[i*4+5],Nsublayer)
+                            Cn2_range = np.linspace(args[i*4+2],args[i*4+6],Nsublayer)
+                            z_range   = np.linspace(args[i*4+3],args[i*4+7],Nsublayer)
+                            for j in range(Nsublayer):
+                                #Vsigma=z_range[j]/7
+                                term = gamma_se1(x, y, t_delta, Vx_range[j], Vy_range[j], Cn2_range[j]/Nsublayer, z_range[j])
+                                #arr += gaussian(term, sigma=Vsigma*t_delta).ravel()
+                                arr += term.ravel()
+                            else:
+                                term = gamma_se1(x, y, t_delta, Vx_range[j], Vy_range[j], Cn2_range[j]/Nsublayer, z_range[j])
+                                arr += term.ravel()
+                    else:
+                        arr = np.zeros(x.shape, dtype=np.float32)
+                        if self.use_windvar: 
+                            for i in range(len(args)//5):
+                                arr += gamma_se2(x, y, t_delta, args[i*5], args[i*5+1], args[i*5+2], args[i*5+3], args[i*5+4]).ravel()
+                        else:
+                            for i in range(len(args)//4):
+                                arr += gamma_se1(x, y, t_delta, args[i*4], args[i*4+1], args[i*4+2], args[i*4+3]).ravel()
+                    
+                    total_cc[idx*n_elem:(idx+1)*n_elem] = arr
+                if hasattr(self,'mask'):
+                    total_cc *= self.mask.ravel()
+                return total_cc
 
         p0 = [p for prms in initial_params for p in prms]
 
-        x = np.linspace(-data.shape[1]//2, data.shape[1]//2-1, data.shape[1], dtype=np.float32)
-        y = np.linspace(-data.shape[0]//2, data.shape[0]//2-1, data.shape[0], dtype=np.float32)
+        x = np.linspace(-data.shape[2]//2, data.shape[2]//2-1, data.shape[2], dtype=np.float32)
+        y = np.linspace(-data.shape[1]//2, data.shape[1]//2-1, data.shape[1], dtype=np.float32)
         X, Y = np.meshgrid(x, y)
+        # mask = np.zeros(data.shape)
+        # mask[0] = (X>-10)*(X<10)*(Y<10)*(Y>-10)
+        # data *= mask
 
-        fit = np.zeros(X.shape, dtype=np.float32) 
         xdata = np.vstack((X.ravel(), Y.ravel())) 
         ydata = data.ravel()
-        
+
+        xdata = np.tile(xdata,len(latency))
+
         # более точные баунсы для начальных параметров
-        lb2 = np.zeros((len(p0)//4, 4), dtype=np.float32)
-        ub2 = np.zeros((len(p0)//4, 4), dtype=np.float32)
-        for i in range(len(all_Vx)):
-            if  i == dome_index:
-                lb2[i] = [all_Vx[i]-0.5, all_Vy[i]-0.5, all_Cn2_bounds[i][0]-0.005, conjugated_distance-(conjugated_distance*0.02)]
-                ub2[i] = [all_Vx[i]+0.5, all_Vy[i]+0.5, all_Cn2_bounds[i][1]+0.005, conjugated_distance+(conjugated_distance*0.02)]
+        if do_fitting:
+            if use_windvar:
+                lb2 = np.zeros((len(p0)//5, 5), dtype=np.float32)
+                ub2 = np.zeros((len(p0)//5, 5), dtype=np.float32)
             else:
-                lb2[i] = [all_Vx[i]-0.5, all_Vy[i]-0.5, all_Cn2_bounds[i][0]-0.005, conjugated_distance]
-                ub2[i] = [all_Vx[i]+0.5, all_Vy[i]+0.5, all_Cn2_bounds[i][1]+0.005, 50]
-        lb2 = np.ravel(lb2)
-        ub2 = np.ravel(ub2)
-        
+                lb2 = np.zeros((len(p0)//4, 4), dtype=np.float32)
+                ub2 = np.zeros((len(p0)//4, 4), dtype=np.float32)
+            for i in range(len(all_Vx)):
+#                 if use_windvar:
+#                     lb2[i] = [all_Vx[i]-0.5, all_Vy[i]-0.5, all_Cn2_bounds[i][0]-0.5, conjugated_distance, 0]
+#                     ub2[i] = [all_Vx[i]+0.5, all_Vy[i]+0.5, all_Cn2_bounds[i][1]+0.5, 50, 2]
+#                 else:
+#                     lb2[i] = [all_Vx[i]-0.5, all_Vy[i]-0.5, all_Cn2_bounds[i][0]-0.5, conjugated_distance]
+#                     ub2[i] = [all_Vx[i]+0.5, all_Vy[i]+0.5, all_Cn2_bounds[i][1]+0.5, 50]
+                if  i == dome_index:
+                    if use_windvar:
+                        lb2[i] = [all_Vx[i]-0.5, all_Vy[i]-0.5, all_Cn2_bounds[i][0]-0.005, conjugated_distance-(conjugated_distance*0.01), 0]
+                        ub2[i] = [all_Vx[i]+0.5, all_Vy[i]+0.5, all_Cn2_bounds[i][1]+0.005, conjugated_distance+(conjugated_distance*0.01), 2]
+                    else:
+                        lb2[i] = [all_Vx[i]-0.5, all_Vy[i]-0.5, all_Cn2_bounds[i][0]-0.005, conjugated_distance-(conjugated_distance*0.01)]
+                        ub2[i] = [all_Vx[i]+0.5, all_Vy[i]+0.5, all_Cn2_bounds[i][1]+0.005, conjugated_distance+(conjugated_distance*0.01)]
+                else:
+                    if use_windvar:
+                        lb2[i] = [all_Vx[i]-0.5, all_Vy[i]-0.5, all_Cn2_bounds[i][0]-0.005, conjugated_distance, 0]
+                        ub2[i] = [all_Vx[i]+0.5, all_Vy[i]+0.5, all_Cn2_bounds[i][1]+0.005, 50, 2]
+                    else:
+                        lb2[i] = [all_Vx[i]-0.5, all_Vy[i]-0.5, all_Cn2_bounds[i][0]-0.005, conjugated_distance]
+                        ub2[i] = [all_Vx[i]+0.5, all_Vy[i]+0.5, all_Cn2_bounds[i][1]+0.005, 50]
+            lb2=np.ravel(lb2)
+            ub2=np.ravel(ub2)
+
 #         lb = [-np.inf, -np.inf, 0, conjugated_distance]
 #         lb = np.tile(lb, len(p0)//4) 
 #         ub = [np.inf, np.inf, np.inf, 50]
 #         ub = np.tile(ub, len(p0)//4)
-     
-        popt, pcov = curve_fit(_g, xdata, ydata, p0, bounds=[lb2, ub2])
 
-        for i in range(len(popt)//4):
-            fit += gamma_se(X, Y, *popt[i*4:i*4+4])
+# БС: Класс позволяет передавать аппроксимирующей функции дополнительные параметры, 
+# в данном случае используем ли мы градиент между слоями или нет.
+# Такая реализация лучше, поскольку мы концентрируем сложение слоев в одной функции _g.fitfun
+        _g = _g()
+        _g.use_gradient = use_gradient
+        _g.latency = latency
+        _g.use_windvar = use_windvar
+        # сюда добавить маску с моим флагом
+        if dome_only != 0:
+            mask = circle(dome_only, data.shape[1], circle_centre=(0, 0), origin="middle").ravel()
+            mask = np.tile(mask, len(latency))
+            _g.mask = mask
+
+# БС: считаем невязку для начального приближения
+        fit_p0 = fitconvert(_g.fitfun(xdata,*p0),data.shape)
+
+        residual_p0 = np.sum(np.power(data-fit_p0,2))
+        print(f'residual for initial guess:{residual_p0}')
+
+        if do_fitting:
+            popt, pcov = curve_fit(_g.fitfun, xdata, ydata, p0, bounds=[lb2, ub2])   
+        else:
+            popt = np.array(p0)
+
+        fit = fitconvert(_g.fitfun(xdata,*popt),data.shape)
+
+#        for i in range(len(popt)//4):
+#            fit += gamma_se(X, Y, *popt[i*4:i*4+4])
 
 #     #     errors = np.sqrt(np.diag(pcov))
-
-        popt = popt.reshape(len(popt)//4, 4)
-
-        df = pd.DataFrame(popt, columns = ['Vx, m/s','Vy, m/s','Cn2', 'z, m'])
+        if use_windvar:
+            popt = popt.reshape(len(popt)//5, 5)
+            df = pd.DataFrame(popt, columns = ['Vx, m/s','Vy, m/s','Cn2', 'z, m', 'var, m/s'])     
+        else:
+            popt = popt.reshape(len(popt)//4, 4)
+            df = pd.DataFrame(popt, columns = ['Vx, m/s','Vy, m/s','Cn2', 'z, m'])
+            
         df = df.sort_values(by=['z, m'])
         df = df.reset_index()
         df['Cn2'] = df['Cn2']*1e-13
@@ -194,8 +308,7 @@ def processApprox(cc=None, gammas=None, lambda_=None, D=None, latency=None, sec_
         df = df.round({'Vx, m/s': 2})
         df = df.round({'Vy, m/s': 2})
         df = df.round({'z, m': 2})
-        df.drop(columns=['index'], inplace=True)
-
+        df.drop(columns=['index'], inplace=True)   
         sum_cn2 = np.sum(df['Cn2'])        
         r0 = pow(0.423 * pow((2*np.pi/lambda_), 2) * sum_cn2, -3/5)
         seeing = 206265 * 0.98 * lambda_/r0
@@ -203,25 +316,33 @@ def processApprox(cc=None, gammas=None, lambda_=None, D=None, latency=None, sec_
         print(df.to_string(index=False))
         print(' - total Cn2:', sum_cn2)
         print(f' - seeing, {lambda_/1e-9:.0f} nm: {seeing:.2f}')
-        
+
+# БС: считаем невязку
+        print(fit.shape)
+        mask = np.zeros(fit.shape)
+        residual = np.sum(np.power(data-fit,2))
+        print(f'total residual:{residual}')
+
         return fit
     
     # изображение кросс-корреляции делится на cjk, чтобы в аппроксимации не учитывать его домножение
 
-    fig, (ax, ax2, ax3) = plt.subplots(1, 3, figsize=(25, 5))
-    cc=cc/cjk
-    cc[np.isinf(cc)] = 0
-    fig.colorbar(ax.imshow(cc), ax=ax)
-    fig.colorbar(ax2.imshow(circle(cc.shape[0]//2-3,cc.shape[0])), ax=ax2)
-    cc = cc * circle(cc.shape[0]//2-3,cc.shape[0])
-    # Для составления изображения весов для кервфита можно будет за пределами круга поменять 0 на 1е8 
-    fig.colorbar(ax3.imshow(cc), ax=ax3)
-    ax.set_title('cc/cjk')
-    ax2.set_title('circle')
-    ax3.set_title('cc/cjk * circle')
+    # fig, (ax, ax2, ax3) = plt.subplots(1, 3, figsize=(25, 5))
+    # cc=cc/cjk
+    # cc[np.isinf(cc)] = 0
+    # fig.colorbar(ax.imshow(cc), ax=ax)
+    # fig.colorbar(ax2.imshow(circle(cc.shape[0]//2-3,cc.shape[0])), ax=ax2)
+    # cc = cc * circle(cc.shape[0]//2-3,cc.shape[0])
+    # # Для составления изображения весов для кервфита можно будет за пределами круга поменять 0 на 1е8 
+    # fig.colorbar(ax3.imshow(cc), ax=ax3)
+    # ax.set_title('cc/cjk')
+    # ax2.set_title('circle')
+    # ax3.set_title('cc/cjk * circle')
     
-    fit = one_speckle_fit(initial_params=initial_params, data=cc, lambda_=lambda_, all_Vx=all_Vx, all_Vy=all_Vy, all_Cn2_bounds=all_Cn2_bounds, conjugated_distance=conjugated_distance, dome_index=dome_index)
+    fit = one_speckle_fit(initial_params=initial_params, data=cc, latency=latency, lambda_=lambda_, all_Vx=all_Vx, all_Vy=all_Vy, all_Cn2_bounds=all_Cn2_bounds, conjugated_distance=conjugated_distance, dome_index=dome_index, use_gradient=use_gradient, do_fitting=do_fitting, dome_only=dome_only, use_windvar=use_windvar)
     
+
+
     return fit
 #         print()
 #         return fit, popt
--- main.py
+++ main.py
@@ -9,10 +9,11 @@ from initialparams import processBestThresh, processPeakDetect, processCoordsToS
 from approx import processApprox 
 from checkfiles import processCheckFiles
 
-def processDomecam(file=None, file_bias=None, data_dir=None, D=None, conjugated_distance=None, latency=None, spectrum=None, lambda_=None, file_star=None, file_filter=None, file_ccd=None):
+def processDomecam(file=None, file_bias=None, data_dir=None, D=None, conjugated_distance=None, latency=None, spectrum=None, lambda_=None, file_star=None, file_filter=None, file_ccd=None, initial_params=None, use_gradient=None, do_fitting=None, dome_only=None, use_windvar=None):
     # считывание данных, получение кросс-корр и автокорреляции зрачка 
-    metka = processCheckFiles(file=file, latency=latency, data_dir=data_dir)
-    cc, cjk, sec_per_frame = processCorr(run_cc=metka, file=file, file_bias=file_bias, D=D, latency=latency, data_dir=data_dir)
+    metka = processCheckFiles(file=file, latency=latency, data_dir=data_dir, dome_only=dome_only)
+    cc, cjk, sec_per_frame = processCorr(run_cc=metka, file=file, file_bias=file_bias, D=D, latency=latency, data_dir=data_dir, dome_only=dome_only)
+    
     # cc - картина кросс-корреляции
     # cjk - картина автокорреляции зрачка 
     # sec_per_frame - период между кадрами, [секунда]
@@ -29,50 +30,91 @@ def processDomecam(file=None, file_bias=None, data_dir=None, D=None, conjugated_
     print(f' - time: {time.perf_counter() - st:.2f}')
     
     # подсчет начальных параметров для аппроксимации 
-    for latency_i in range(len(latency)):
-        print('calculation initial parametrs')
-        st = time.perf_counter()
-        thresh = processBestThresh(cc[latency_i], acc=5)
-        y, x = processPeakDetect(cc[latency_i] * (cc[latency_i]>thresh), size_of_neighborhood=7)
-        all_Vy, all_Vx = processCoordsToSpeed(y, x, latency=latency[latency_i], sec_per_frame=sec_per_frame, D=D, cc=cc[latency_i])
-        all_Cn2_bounds, all_Cn2_mean = processCn2(cc[latency_i]/cjk, y, x, gammas, conjugated_distance=conjugated_distance, heights_of_layers=heights_of_layers)
-        
-        initial_params = np.zeros((len(x), 4), dtype=np.float32)
-        for i in range(len(x)):
-            if int(all_Vx[i])==0 and int(all_Vy[i])==0:
-                # тут можно для Cn2 брать значение Cn2 для conjugated_distance, а не Cn2_mean
-                initial_params[i] = [all_Vx[i], all_Vy[i], all_Cn2_bounds[i][1], conjugated_distance]
-                dome_index = i
+    # БС: начальные параметры будем определять по кросс-корряляции, посчитанной для минимальной задержки
+    # БС: если начальные параметры были введены при вызове processDomecam, не оценивать их
+    if do_fitting:
+        for latency_i in [0]:
+            print('calculation initial parametrs')
+            st = time.perf_counter()
+            thresh = processBestThresh(cc[latency_i], acc=5)
+            y, x = processPeakDetect(cc[latency_i] * (cc[latency_i]>thresh), size_of_neighborhood=7)
+            all_Vy, all_Vx = processCoordsToSpeed(y, x, latency=latency[latency_i], sec_per_frame=sec_per_frame, D=D, cc=cc[latency_i])
+            all_Cn2_bounds, all_Cn2_mean = processCn2(cc[latency_i]/cjk, y, x, gammas, conjugated_distance=conjugated_distance, heights_of_layers=heights_of_layers)
+            
+            if use_windvar:
+                initial_params = np.zeros((len(x), 5), dtype=np.float32)
+                for i in range(len(x)):
+                    if int(all_Vx[i])==0 and int(all_Vy[i])==0:
+                        # тут можно для Cn2 брать значение Cn2 для conjugated_distance, а не Cn2_mean
+                        initial_params[i] = [all_Vx[i], all_Vy[i], all_Cn2_bounds[i][1], conjugated_distance, 2]
+                        dome_index = i
+                    else:
+                        initial_params[i] = [all_Vx[i], all_Vy[i], all_Cn2_mean[i], 10, 2]
             else:
-                initial_params[i] = [all_Vx[i], all_Vy[i], all_Cn2_mean[i], 10]
-        print(f' - threshold: {thresh:.4f}; {len(y)} peaks found')
-        print(f' - time: {time.perf_counter() - st:.2f}')
-        # thresh - оптимальный трешхолд картины кросс-корреляции 
-        # y, x - координаты пиков
-        # all_Vy, all_Vx - скорости этих же пиков, [м/с]
-        # all_Cn2_bounds - минимальное и максимальное значение интенсивонсти для каждого найденного пика
-        # initial_params - начальные параметры для каждого пика
-        
+                initial_params = np.zeros((len(x), 4), dtype=np.float32)
+                for i in range(len(x)):
+                    if int(all_Vx[i])==0 and int(all_Vy[i])==0:
+                        # тут можно для Cn2 брать значение Cn2 для conjugated_distance, а не Cn2_mean
+                        initial_params[i] = [all_Vx[i], all_Vy[i], all_Cn2_bounds[i][1], conjugated_distance]
+                        dome_index = i
+                    else:
+                        initial_params[i] = [all_Vx[i], all_Vy[i], all_Cn2_mean[i], 10]
+            print(f' - threshold: {thresh:.4f}; {len(y)} peaks found')
+            print(f' - time: {time.perf_counter() - st:.2f}')
+            # thresh - оптимальный трешхолд картины кросс-корреляции 
+            # y, x - координаты пиков
+            # all_Vy, all_Vx - скорости этих же пиков, [м/с]
+            # all_Cn2_bounds - минимальное и максимальное значение интенсивонсти для каждого найденного пика
+            # initial_params - начальные параметры для каждого пика
+            
+            fig, (ax, ax2, ax3) = plt.subplots(1, 3, figsize=(25, 5))
+            ax.scatter(x, y, c='red', marker='x', s=1)
+            fig.colorbar(ax.imshow(cc[latency_i]), ax=ax)
+            fig.colorbar(ax2.imshow(cc[latency_i] * (cc[latency_i]>thresh)), ax=ax2)
+            fig.colorbar(ax3.imshow(cjk), ax=ax3)
+            ax.set_title('Найденные пики')
+            ax2.set_title('Оптимальный трешхолд')
+            ax3.set_title('Автокорреляция зрачка')
+            fig.suptitle('Вспомогательные картинки')
+    else:
+        dome_index = 0
+        all_Vx = None
+        all_Vy = None
+        all_Cn2_bounds = None
+        all_Cn2_mean = None
+        all_Cn2_bounds = None
+
+    # БС: аппроксимацию выполняем по кросс-корреляциям, соответствующим всем задержкам одновременно        
+    print('approxing')
+    st=time.perf_counter()
+    fit = processApprox(cc=cc, gammas=gammas, lambda_=lambda_, D=D, latency=latency, sec_per_frame=sec_per_frame, cjk=cjk, 
+                        initial_params=initial_params, all_Vx=all_Vx, all_Vy=all_Vy, all_Cn2_bounds=all_Cn2_bounds, 
+                        conjugated_distance=conjugated_distance, num_of_layers=num_of_layers, heights_of_layers=heights_of_layers, 
+                        dome_index=dome_index, use_gradient=use_gradient, do_fitting=do_fitting, dome_only=dome_only, use_windvar=use_windvar)
+    print(f' - time: {time.perf_counter() - st:.2f}')
+
+    # БС: отображение результатов аппроксимации        
+    xc=226
+    if dome_only != 0:
+        xs = 20
+    if dome_only == 0:
+        xs = 220
+    xlims=(xc-xs,xc+xs)
+    ylims=(xc+xs,xc-xs)
+    vmin=-0.002
+    vmax=0.008
+    for latency_i in range(len(latency)):
         fig, (ax, ax2, ax3) = plt.subplots(1, 3, figsize=(25, 5))
-        ax.scatter(x, y, c='red', marker='x', s=1)
-        fig.colorbar(ax.imshow(cc[latency_i]), ax=ax)
-        fig.colorbar(ax2.imshow(cc[latency_i] * (cc[latency_i]>thresh)), ax=ax2)
-        fig.colorbar(ax3.imshow(cjk), ax=ax3)
-        ax.set_title('Найденные пики')
-        ax2.set_title('Оптимальный трешхолд')
-        ax3.set_title('Автокорреляция зрачка')
-        fig.suptitle('Вспомогательные картинки')
-        
-        print('approxing')
-        st=time.perf_counter()
-        fit = processApprox(cc=cc[latency_i], gammas=gammas, lambda_=lambda_, D=D, latency=latency[latency_i], sec_per_frame=sec_per_frame, cjk=cjk, initial_params=initial_params, all_Vx=all_Vx, all_Vy=all_Vy, all_Cn2_bounds=all_Cn2_bounds, conjugated_distance=conjugated_distance, num_of_layers=num_of_layers, heights_of_layers=heights_of_layers, dome_index=dome_index)
-        print(f' - time: {time.perf_counter() - st:.2f}')
-        
-        fig, (ax, ax2, ax3) = plt.subplots(1, 3, figsize=(25, 5))
-        fig.colorbar(ax.imshow(cc[latency_i]), ax=ax)
-        fig.colorbar(ax2.imshow(fit*cjk), ax=ax2)
-        fig.colorbar(ax3.imshow(cc[latency_i]-(fit*cjk)), ax=ax3)
-        ax.set_title('orig')
-        ax2.set_title('model')
-        ax3.set_title('resid')
+        fig.colorbar(ax.imshow(cc[latency_i], vmin=vmin, vmax=vmax), ax=ax)
+        fig.colorbar(ax2.imshow(fit[latency_i]*cjk, vmin=vmin, vmax=vmax), ax=ax2)
+        fig.colorbar(ax3.imshow((cc[latency_i]-fit[latency_i]*cjk), vmin=-0.004, vmax=0.004), ax=ax3)
+        ax.set_title(f'orig, lat={latency[latency_i]}')
+        ax.set_xlim(xlims)
+        ax.set_ylim(ylims)
+        ax2.set_title(f'model, lat={latency[latency_i]}')
+        ax2.set_xlim(xlims)
+        ax2.set_ylim(ylims)
+        ax3.set_title(f'resid, lat={latency[latency_i]}')
+        ax3.set_xlim(xlims)
+        ax3.set_ylim(ylims)
   
