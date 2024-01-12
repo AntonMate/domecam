dc115757033a2500b0711a500bb13d15e786089d
AntonMate <mironov1424@gmail.com>
2024-01-12 16:48:36 +0300

    added arguments func

--- domecam_process_batch.py
+++ domecam_process_batch.py
@@ -1,10 +1,30 @@
 from main import processDomecam
 import os
 import getopt
+import sys
 
-file = '20210130l_m2km.fits'
-file_bias = 'bias20210130.fits'
-data_dir = 'D:/astro/domecam' # рабочая папка, все исходные файлы должны храниться в ней
+# ============================================================================
+optlist, args = getopt.getopt(sys.argv[1:], 'infile', ['infile='])
+new_path = optlist[0][1]
+print(new_path)
+print('этот файл есть?', os.path.isfile(new_path))
+indexes = [i for i in range(len(new_path)) if new_path[i] == "\\"]
+data_dir = new_path[:indexes[-1]]
+print('дир:', data_dir)
+file = new_path[indexes[-1]+1:]
+print('файл:', file)
+file_name = file.replace('_m2km.fits', '')
+print('номер серии:', file_name)
+
+for item in os.listdir(data_dir):
+    if 'bias' in item and file_name in item:
+        print('есть ли для него bias?', item)
+        file_bias = item
+# ============================================================================
+
+# file = '20210130l_m2km.fits'
+# file_bias = 'bias20210130.fits'
+# data_dir = 'D:/astro/domecam' # рабочая папка, все исходные файлы должны храниться в ней
 
 latency = [4, 6] # задержка для кросс-корреляции, [кадр]
 conjugated_distance = 2 # сопряженное расстояние, [километр]
@@ -35,7 +55,7 @@ dome_only = 5 # 0, чтобы отключить. >0, чтобы задать р
 #                   [  -17.26 ,  -16.99, 3*3.670290, 10.65737]]
 #initial_params = None
 
-for file in os.listdir(f'{data_dir}'):
+for file in os.listdir(data_dir):
     if file.endswith('.fits') and not file.startswith('bias'):
         print(file[:-5])
         processDomecam(file=file, file_bias=file_bias, data_dir=data_dir, 
