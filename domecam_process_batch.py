09c026d018406de0329945343e8157b1d80b013b
AntonMate <mironov1424@gmail.com>
2024-01-21 16:17:46 +0300

    all files in folder process

--- domecam_process_batch.py
+++ domecam_process_batch.py
@@ -4,65 +4,6 @@ import getopt
 import sys
 
 # ============================================================================
-optlist, args = getopt.getopt(sys.argv[1:], 'infile', ['infile='])
-new_path = optlist[0][1]
-print(f'---обработка {new_path}')
-
-if new_path.endswith('.fits'):
-    print('---этот файл есть?', os.path.isfile(new_path))
-    indexes = [i for i in range(len(new_path)) if new_path[i] == "/"]
-    data_dir = new_path[:indexes[-1]]
-    print('---дир:', data_dir)
-    file = new_path[indexes[-1]+1:]
-    print('---файл:', file)
-    file_name = file.replace('_2km.fits', '')
-    file_name = file_name.replace('_0km.fits', '')
-    print('---номер серии:', file_name)
-
-    indexes_h = [i for i in range(len(file)) if file[i] == "k"]
-    print('---высота сопряжения:', file[indexes_h[0]-1])
-
-    for item in os.listdir(data_dir):
-        if 'bias' in item and file_name in item and f'{file[indexes_h[0]-1]}km' in item:
-            print('---есть ли для него bias?', item)
-            file_bias = item
-
-    with open('logs.txt') as f:
-        for line in f:
-            if file in line:
-                print('---из файла logs.txt:', line.strip())
-                file_star = f'{line.split()[2].lower()}.sp'
-                print(f'---файл со спектром: {file_star}')
-                
-else:
-    print(f'---обработка всех файлов из папки {new_path}')
-    data_dir = new_path
-    print('---дир:', data_dir)
-    for ser in os.listdir(new_path):
-        if ser.endswith('_2km.fits'):
-            file = ser
-            print('---файл:', file)
-            file_name = file.replace('_2km.fits', '')
-            print('---номер серии:', file_name)
-            indexes_h = [i for i in range(len(file)) if file[i] == "k"]
-            print('---высота сопряжения:', file[indexes_h[0]-1])
-            for item in os.listdir(data_dir):
-                if 'bias' in item and file_name in item and f'{file[indexes_h[0]-1]}km' in item:
-                    print('---есть ли для него bias?', item)
-                    file_bias = item
-                    
-            with open('logs.txt') as f:
-                for line in f:
-                    if file in line:
-                        print('---из файла logs.txt:', line.strip())
-                        file_star = f'{line.split()[2].lower()}.sp'
-                        print(f'---файл со спектром: {file_star}')
-# ============================================================================
-
-# file = '20210130l_m2km.fits'
-# file_bias = 'bias20210130.fits'
-# data_dir = 'D:/astro/domecam' # рабочая папка, все исходные файлы должны храниться в ней
-
 latency = [4, 6] # задержка для кросс-корреляции, [кадр]
 conjugated_distance = 2 # сопряженное расстояние, [километр]
 D = 2.5 # диаметр телескопа, [метр]
@@ -90,10 +31,65 @@ dome_only = 5 # 0, чтобы отключить. >0, чтобы задать р
 #                   [   -6.38 ,   -6.27, 5*1.314183,  3.19194],
 #                   [  -11.04 ,  -10.69, 2*1.060673,  9.83704],
 #                   [  -17.26 ,  -16.99, 3*3.670290, 10.65737]]
-#initial_params = None
 
-# for file in os.listdir(data_dir):
-#     if file.endswith('.fits') and not file.startswith('bias'):
-#         print(file[:-5])
 
-processDomecam(file=file, file_name=file_name, file_bias=file_bias, data_dir=data_dir, D=D, conjugated_distance=conjugated_distance, latency=latency, spectrum=spectrum, lambda_=lambda_, file_filter=file_filter, file_ccd=file_ccd, file_star=file_star, do_fitting=do_fitting, use_gradient=use_gradient, initial_params=None, dome_only=dome_only, use_windvar=use_windvar)
+# ============================================================================
+optlist, args = getopt.getopt(sys.argv[1:], 'infile', ['infile='])
+new_path = optlist[0][1]
+
+if new_path.endswith('.fits'):
+    print(f'---обработка {new_path}')
+    print('---этот файл есть?', os.path.isfile(new_path))
+    indexes = [i for i in range(len(new_path)) if new_path[i] == "/"]
+    data_dir = new_path[:indexes[-1]]
+    print('---дир:', data_dir)
+    file = new_path[indexes[-1]+1:]
+    print('---файл:', file)
+    file_name = file.replace('_2km.fits', '')
+    file_name = file_name.replace('_0km.fits', '')
+    print('---номер серии:', file_name)
+
+    indexes_h = [i for i in range(len(file)) if file[i] == "k"]
+    print('---высота сопряжения:', file[indexes_h[0]-1])
+
+    for item in os.listdir(data_dir):
+        if 'bias' in item and file_name in item and f'{file[indexes_h[0]-1]}km' in item:
+            print('---есть ли для него bias?', item)
+            file_bias = item
+
+    with open('logs.txt') as f:
+        for line in f:
+            if file in line:
+                print('---из файла logs.txt:', line.strip())
+                file_star = f'{line.split()[2].lower()}.sp'
+                print(f'---файл со спектром: {file_star}')
+    
+    processDomecam(file=file, file_name=file_name, file_bias=file_bias, data_dir=data_dir, D=D, conjugated_distance=conjugated_distance, latency=latency, spectrum=spectrum, lambda_=lambda_, file_filter=file_filter, file_ccd=file_ccd, file_star=file_star, do_fitting=do_fitting, use_gradient=use_gradient, initial_params=None, dome_only=dome_only, use_windvar=use_windvar)
+                
+else:
+    print(f'---обработка всех файлов из {new_path}')
+    data_dir = new_path
+    print('---дир:', data_dir)
+    for ser in os.listdir(new_path):
+        if ser.endswith('_2km.fits'):
+            file = ser
+            print('---файл:', file)
+            file_name = file.replace('_2km.fits', '')
+            print('---номер серии:', file_name)
+            indexes_h = [i for i in range(len(file)) if file[i] == "k"]
+            print('---высота сопряжения:', file[indexes_h[0]-1])
+            for item in os.listdir(data_dir):
+                if 'bias' in item and file_name in item and f'{file[indexes_h[0]-1]}km' in item:
+                    print('---есть ли для него bias?', item)
+                    file_bias = item
+                    
+            with open('logs.txt') as f:
+                for line in f:
+                    if file in line:
+                        print('---из файла logs.txt:', line.strip())
+                        file_star = f'{line.split()[2].lower()}.sp'
+                        print(f'---файл со спектром: {file_star}')
+            
+            processDomecam(file=file, file_name=file_name, file_bias=file_bias, data_dir=data_dir, D=D, conjugated_distance=conjugated_distance, latency=latency, spectrum=spectrum, lambda_=lambda_, file_filter=file_filter, file_ccd=file_ccd, file_star=file_star, do_fitting=do_fitting, use_gradient=use_gradient, initial_params=None, dome_only=dome_only, use_windvar=use_windvar)
+# ============================================================================
+
