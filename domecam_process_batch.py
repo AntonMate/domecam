28aa6b728b2e646ee8d9a15d8a973322a112705f
AntonMate <mironov1424@gmail.com>
2024-01-24 15:32:58 +0300

    fixed result txt file

--- domecam_process_batch.py
+++ domecam_process_batch.py
@@ -9,30 +9,28 @@ conjugated_distance = 2 # сопряженное расстояние, [кило
 D = 2.5 # диаметр телескопа, [метр]
 spectrum = 'poly' # тип излучения
 lambda_ = 650*1e-9 # длина наблюдаемой волны света, [метр]
- 
-use_gradient = False # БС: использовать ли градиенты между слоями
-use_windvar = True # использовать ли дисперсию ветра
 
 # для полихроматического излучения нужны кривые фильтра, детектора и звезды
 file_filter = 'KC19_d16t4_Safonov.xls'
 file_ccd = 'ccd_prosilica2.crv'
 # file_star = 'a05.sp'
 
+use_gradient = False # БС: использовать ли градиенты между слоями
+use_windvar = True # использовать ли дисперсию ветра
+
 # БС: если параметр do_fitting равен False: отладочный режим, аппроксимация не будет выполнена, а будут взяты начальные 
 # параметры initial_params
 # БС: если параметр do_fitting равен True, то будет выполнена оценка начальных параметров и проведена аппроксимация
 do_fitting = True
 dome_only = 5 # 0, чтобы отключить. >0, чтобы задать радиус области вокруг центра
-
+input_parametrs = [[0, 0, 2*0.5495173, 2, 0.25]]
+# ============================================================================
+if do_fitting == True:
+    initial_params = None
+else:
+    initial_params = input_parametrs
 # начальные параметры для аппроксимации в явном виде
 # если их не указать, то они будут подобраны автоматически
-# initial_params = [[   -0.03 ,    0.02, 2*0.5495173, 1.96746],
-#                   [   -4.69 ,   -2.26, 5*1.019672,  3.23578],
-#                   [   -6.38 ,   -6.27, 5*1.314183,  3.19194],
-#                   [  -11.04 ,  -10.69, 2*1.060673,  9.83704],
-#                   [  -17.26 ,  -16.99, 3*3.670290, 10.65737]]
-
-
 # ============================================================================
 optlist, args = getopt.getopt(sys.argv[1:], 'infile', ['infile='])
 new_path = optlist[0][1]
@@ -61,7 +59,7 @@ if new_path.endswith('.fits'):
                 file_star = f'{line.split()[2].lower()}.sp'
                 print(f' - spectrum: {file_star}')
     
-    processDomecam(file=file, file_name=file_name, file_bias=file_bias, data_dir=data_dir, D=D, conjugated_distance=conjugated_distance, latency=latency, spectrum=spectrum, lambda_=lambda_, file_filter=file_filter, file_ccd=file_ccd, file_star=file_star, do_fitting=do_fitting, use_gradient=use_gradient, initial_params=None, dome_only=dome_only, use_windvar=use_windvar, star_name=star_name)
+    processDomecam(file=file, file_name=file_name, file_bias=file_bias, data_dir=data_dir, D=D, conjugated_distance=conjugated_distance, latency=latency, spectrum=spectrum, lambda_=lambda_, file_filter=file_filter, file_ccd=file_ccd, file_star=file_star, do_fitting=do_fitting, use_gradient=use_gradient, initial_params=initial_params, dome_only=dome_only, use_windvar=use_windvar, star_name=star_name, latency_list=latency)
                 
 else:
     print(f' - MODE: обработка всех файлов _2km.fits из {new_path}')
@@ -86,6 +84,6 @@ else:
                         file_star = f'{line.split()[2].lower()}.sp'
                         print(f' - spectrum: {file_star}')
             
-            processDomecam(file=file, file_name=file_name, file_bias=file_bias, data_dir=data_dir, D=D, conjugated_distance=conjugated_distance, latency=latency, spectrum=spectrum, lambda_=lambda_, file_filter=file_filter, file_ccd=file_ccd, file_star=file_star, do_fitting=do_fitting, use_gradient=use_gradient, initial_params=None, dome_only=dome_only, use_windvar=use_windvar, star_name=star_name)
+            processDomecam(file=file, file_name=file_name, file_bias=file_bias, data_dir=data_dir, D=D, conjugated_distance=conjugated_distance, latency=latency, spectrum=spectrum, lambda_=lambda_, file_filter=file_filter, file_ccd=file_ccd, file_star=file_star, do_fitting=do_fitting, use_gradient=use_gradient, initial_params=initial_params, dome_only=dome_only, use_windvar=use_windvar, star_name=star_name, latency_list=latency)
 # ============================================================================
 
