9f19c39198ff4eafe5e644114dd862cb4b5701b3
AntonMate <mironov1424@gmail.com>
2024-01-24 19:01:04 +0300

    add sql for telescope temp15

--- sql_telescope_temp.py
+++ sql_telescope_temp.py
@@ -83,7 +83,7 @@ def all_info_from_sql(data_dir, file_name, file, file_time, file_time_ub):
         conn.close()
         return ts_1, ts_2, ts_3, ts_4, ts_5, ts_6, ts_7, ts_8, ts_9, ts_10, ts_11, ts_12, ts_13, ts_14, ts_15, ts_16, ts_17, ts_19
     
-    result_temperature, result_wind_direction, result_wind_speed = speed_direction_temperature(data_dir=data_dir)
+    result_temperature, result_wind_direction, result_wind_speed = speed_direction_temperature(data_dir=data_dir, file_time, file_time_ub)
     ts_1, ts_2, ts_3, ts_4, ts_5, ts_6, ts_7, ts_8, ts_9, ts_10, ts_11, ts_12, ts_13, ts_14, ts_15, ts_16, ts_17, ts_19 = telescope_temerarute(file_time=file_time, file_time_ub=file_time_ub)
     
     all_info = [result_temperature, result_wind_direction, result_wind_speed, ts_1, ts_2, ts_3, ts_4, ts_5, ts_6, ts_7, ts_8, ts_9, ts_10, ts_11, ts_12, ts_13, ts_14, ts_15, ts_16, ts_17, ts_19]
