8634c0a826b4efb9539ae8051bcadc18537ef99c
AntonMate <mironov1424@gmail.com>
2024-01-24 17:52:09 +0300

    add sql for telescope temp6

--- sql_telescope_temp.py
+++ sql_telescope_temp.py
@@ -9,6 +9,8 @@ cur = conn.cursor()
 cur.execute("SELECT ts_id,meas_time,value from \"sai2p5_temp\" WHERE (meas_time> now() - INTERVAL '3 HOUR' - INTERVAL '2 MINUTE') AND ((ts_id=1) or (ts_id=2) or (ts_id=3) or (ts_id=4) or (ts_id=5) or (ts_id=6) or (ts_id=7) or (ts_id=8) or (ts_id=9) or (ts_id=10) or (ts_id=11) or (ts_id=12) or (ts_id=13) or (ts_id=14) or (ts_id=15) or (ts_id=16) or (ts_id=17) or (ts_id=18) or (ts_id=19));")
 
 data = cur.fetchall()
-print(data)
+for item in data:
+    if item[0] == 1:
+        print('TS-1:', item[0])
 cur.close()
 conn.close()
