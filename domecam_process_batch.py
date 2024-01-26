b40b464909b7cc831565776757ff5561f2071524
AntonMate <mironov1424@gmail.com>
2024-01-26 15:01:22 +0300

    fixed bias not found8

--- domecam_process_batch.py
+++ domecam_process_batch.py
@@ -68,12 +68,7 @@ if new_path.endswith('.fits'):
     
     file_bias = None
     metka_bias = 'found'
-    for item in os.listdir(data_dir):
-        if item.endswith('_2km_bias.fits'):
-            from astropy.io import fits
-            with fits.open(f'{data_dir}/{file}') as f:
-                print(f.info())
-                
+    for item in os.listdir(data_dir):                
         if 'bias' in item and file_name in item and f'{file[indexes_h[0]-1]}km' in item:
             file_bias = item
             print(f' - {file} --> bias: {item}')
