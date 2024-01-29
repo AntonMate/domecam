6c5b0deda2436ae3fddbad26183c270260515e21
AntonMate <mironov1424@gmail.com>
2024-01-29 14:37:49 +0300

    final file6

--- finalize.py
+++ finalize.py
@@ -7,5 +7,5 @@ data_dir = optlist[0][1]
 
 for item in os.listdir(data_dir):
     if item.startswith('DC'):
-        print('item', os.path.isfile(f'{data_dir}/{item}/{item}_2km_result.txt'))
-        print('item', os.path.isfile(f'{data_dir}/{item}/{item}_2km_info_from_logs.txt'))
+        print(item, os.path.isfile(f'{data_dir}/{item}/{item}_2km_result.txt'))
+        print(item, os.path.isfile(f'{data_dir}/{item}/{item}_2km_info_from_logs.txt'))
