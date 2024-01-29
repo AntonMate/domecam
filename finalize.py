d9681968db321c2e5f7d1ed017df78c92f8e372d
AntonMate <mironov1424@gmail.com>
2024-01-29 14:33:10 +0300

    final file3

--- finalize.py
+++ finalize.py
@@ -5,6 +5,5 @@ import sys
 optlist, args = getopt.getopt(sys.argv[1:], 'infile', ['infile='])
 new_path = optlist[0][1]
 
-indexes = [i for i in range(len(new_path)) if new_path[i] == "/"]
-data_dir = new_path[:indexes[-1]]
+data_dir = new_path
 print(data_dir)
