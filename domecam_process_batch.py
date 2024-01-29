f455898e25928f42c8339291d0c6d12161a6108e
AntonMate <mironov1424@gmail.com>
2024-01-29 13:51:43 +0300

    test shape DC23_3

--- domecam_process_batch.py
+++ domecam_process_batch.py
@@ -86,7 +86,9 @@ if new_path.endswith('.fits'):
                 with fits.open(f'{data_dir}/{file_bias}') as fb:
                     print('bias:', fb.info())
             else:
+                print('(!!!!!!!!!!!!!!!)')
                 if item.startswith('DC2202') or item.startswith('DC2203') or item.startswith('DC2205') or item.startswith('DC2211'):
+                    print('LOOOOOOL')
                     file_bias = 'DC221108182951_2km_bias.fits'
                     metka_bias = 'DC221108182951_2km_bias.fits'
         print(f' - {file} --> bias: not found, took bias: {file_bias}')
